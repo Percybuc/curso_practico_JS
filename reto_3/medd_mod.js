16c1ca4ec7587a620c26067f6383926933e3d384
buendiap <pbuendia97@outlook.com>
2021-08-10 10:11:10 -0500

    Promedio con reduce y for

--- reto_3/medd_mod.js
+++ reto_3/medd_mod.js
@@ -1,3 +1,22 @@
 //media arit o promedio: Todo sumado / cantidad de sumandos
 //Mediana: El valor centrar de una lista ordenada. Si son 2 datos se divide entre 2 (sirve cuando hay datos excepcionales) 
 //Moda: El dato que más se repite
+
+// -----------MEDIA ARIT ----------------
+function calcularPromedio (lista){
+    // let sumaLista = 0;
+    
+    // for (var i = 0; i < lista.length; i ++){
+    //     sumaLista = lista[i] + sumaLista;
+    // }
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+        // Recibe 2 parámetros(1ro el valorAcumulado, 2do el nuevoElemento)
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+    console.log(promedioLista);
+}
