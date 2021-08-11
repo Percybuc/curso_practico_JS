d048195bc9b82f440360f8795a7ab005296009ac
buendiap <pbuendia97@outlook.com>
2021-08-11 11:26:53 -0500

    Ahora usando sort para listas desordenadas

--- reto_3/mediana.js
+++ reto_3/mediana.js
@@ -12,11 +12,11 @@ function calcularPromedio (lista){
 }
 
 let lista1 = [
+    40000000,
     100, 
-    200, 
     500, 
-    800,
-    40000000];
+    200,
+    800];
 
 function esPar (numerito){
     if (numerito % 2 === 0){
@@ -28,12 +28,14 @@ function esPar (numerito){
 
 function calcularMediana (listado){
     
+    let listadoOrdenado = listado.sort((a, b) => a - b);
+    //Para ordenar la lista
     let mitadListado = parseInt(listado.length / 2);
     let mediana;
 
     if (esPar(listado.length)) {
-        const elemento1 = listado[mitadListado - 1];
-        const elemento2 = listado[mitadListado];
+        const elemento1 = listadoOrdenado[mitadListado - 1];
+        const elemento2 = listadoOrdenado[mitadListado];
         const caulcularPromedioE1yE2 = calcularPromedio([
             elemento1, 
             elemento2]);
